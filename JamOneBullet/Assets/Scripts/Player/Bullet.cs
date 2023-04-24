ab2673ded1f2572d3ebf8498b806db1448291321
LastNite98 <75075949+LastNite98@users.noreply.github.com>
2023-04-24 14:15:23 +0200

    bullets

--- JamOneBullet/Assets/Scripts/Player/Bullet.cs
+++ JamOneBullet/Assets/Scripts/Player/Bullet.cs
@@ -12,13 +12,11 @@ public class Bullet : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
-
     private void Start()
     {
         rb2d.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
         Destroy(this.gameObject, 1.5f);
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
--- JamOneBullet/Assets/Scripts/Player/Shoot.cs
+++ JamOneBullet/Assets/Scripts/Player/Shoot.cs
@@ -6,16 +6,38 @@ public class Shoot : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] Transform shootPoint;
+    [SerializeField] private int bulletsAvailable = 1;
+    public float timeReloadRestart;
+    public float timeToReload = 2.5f;
 
+    private void Start()
+    {
+        timeReloadRestart = timeToReload;
+    }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && bulletsAvailable > 0)
         {
             Shooting();
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            LoadAmmo();
         }
     }
     private void Shooting()
     {
         GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+        bulletsAvailable--;
     }
+
+    private void LoadAmmo()
+    {
+
+        if (timeToReload <= 0 && bulletsAvailable == 0)
+        {
+            bulletsAvailable++;
+        }
+    }
+
 }//Class
